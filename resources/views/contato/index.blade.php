b9d7293f64dcd9097566311c1d6ebdf609ec848e
FIRST_ARACELI LAST_CRISTINA <araceli.melo@unidavi.edu.br>
2022-10-06 16:58:28 -0300

    atualização 06-10

--- resources/views/contato/index.blade.php
+++ resources/views/contato/index.blade.php
@@ -2,11 +2,15 @@
 @section('title','Listagem de Contatos')
 @section('content')
     <h1>Listagem de Contatos</h1>
+    <br/>
+    <a class="btn btn-success" href="{{url('contatos/create')}}">Criar</a>
     <ul>
+    <br/>
     @foreach ($contatos as $contato)
        <li>
             <a href="{{url('contatos/'.$contato->id)}}">{{$contato->nome}}</a>
         </li>
     @endforeach
     </ul>
+
 @endsection
