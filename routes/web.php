37a3b110a13c13dbdfc7ebc1d21024969ddc56db
FIRST_ARACELI LAST_CRISTINA <araceli.melo@unidavi.edu.br>
2022-10-18 16:58:55 -0300

    inclusão emprestimo

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContatosController;
 use App\Http\Controllers\LivrosController;
+use App\Http\Controllers\EmprestimosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +27,6 @@ Route::resource('contatos',ContatosController::class);
 
 Route::get('livros/buscar',[LivrosController::class,'buscar']);
 Route::resource('livros',LivrosController::class);
+
+Route::get('emprestimos/buscar',[EmprestimosController::class,'buscar']);
+Route::resource('emprestimos',EmprestimosController::class);
